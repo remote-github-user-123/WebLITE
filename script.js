c28fcba90fef7dbadb49d8dc885179acd50b2c3d
remote-github-user-123 <106466101+remote-github-user-123@users.noreply.github.com>
2022-06-01 15:22:47 +0530

    added run function

--- script.js
+++ script.js
@@ -26,3 +26,15 @@ for(let i of btns){
 
     })
 }
+
+//function to run
+function run(){
+    let html = document.getElementById("inputh").value;
+    let css = document.getElementById("inputc").value;
+    let js = document.getElementById("inputj").value;
+    let startIndex = html.indexOf("<body>")+6;
+    let endIndex = html.indexOf("</body>");
+    html = html.slice(startIndex,endIndex);
+    let outputDiv=document.getElementById("output");
+    outputDiv.innerHTML=html;
+}
